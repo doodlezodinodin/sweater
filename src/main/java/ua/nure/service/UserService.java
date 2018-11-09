e85ba2d60297d9bc65c3e9794f0547fe4a4e25dc
Hrankin Oleksandr <doodlezodinodin@gmail.com>
2018-11-09 17:22:58 +0200

    Started versioning the database using flyway. A default administrator has been added to the database. Added page "profile" with the ability to change the password and mail.

--- src/main/java/ua/nure/service/UserService.java
+++ src/main/java/ua/nure/service/UserService.java
@@ -10,8 +10,8 @@ import ua.nure.dao.UserDao;
 import ua.nure.entity.Role;
 import ua.nure.entity.User;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -44,6 +44,12 @@ public class UserService implements UserDetailsService {
 
         userDao.save(user);
 
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
@@ -54,8 +60,6 @@ public class UserService implements UserDetailsService {
 
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -70,4 +74,70 @@ public class UserService implements UserDetailsService {
 
         return true;
     }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userDao.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userDao.save(user);
+
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
